7db678e10a1fee54709474911303c629743971d4
golbangsaennim <snowman.won@gmail.com>
2020-10-22 13:18:32 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-	name='hello_app'
-	version='1.0'
-	description='python-sample-vscode-flask-tutorial'
+	name='hello_app',
+	version='1.0',
+	description='python-sample-vscode-flask-tutorial',
 	packages=['hello_app']
 )
